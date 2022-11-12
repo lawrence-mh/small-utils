f5842513e4af797073e8a2e6bccaf59badd1c010
lawrence-mh <112795716+lawrence-mh@users.noreply.github.com>
2022-11-12 15:57:03 +0800

    Add files via upload
    Credits: 
    https://stackoverflow.com/questions/65372036/how-to-play-a-sound-in-julia
    https://docs.julialang.org/
    https://juliapackages.com/p/wav

---
+++ studytimers.jl
@@ -0,0 +1,35 @@
+module StudyTimers
+using WAV
+    function TonePlayer()
+        # println("\007\007\007\007\007\007")
+        fs = 7e3
+        t = 0.0:1/fs:prevfloat(0.3)
+        f = 8e2 
+        y = sin.(2pi * f * t) * 0.1 
+        wavplay(y, fs) 
+        wavplay(y, fs) 
+        wavplay(y, fs)
+        wavplay(y, fs) 
+        wavplay(y, fs) 
+        wavplay(y, fs)
+        wavplay(y, fs) 
+        wavplay(y, fs) 
+        wavplay(y, fs)
+    end
+
+    function MinToSec(min) 
+        sec = min * 60
+        return sec
+    end
+
+    function CallBack(timer)
+        TonePlayer()
+    end
+
+    input = readline()
+    duration = parse(Float64, input)
+
+    t = Timer(CallBack, MinToSec(duration))
+    wait(t)
+
+end
