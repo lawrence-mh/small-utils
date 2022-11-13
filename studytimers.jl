329145eb5953d863c87f75ec0334fbfdb2c949bc
Laurentino L <101549371+Opportunitycoster@users.noreply.github.com>
2022-11-13 12:39:43 +0800

    Update studytimers.jl

--- studytimers.jl
+++ studytimers.jl
@@ -1,41 +1,43 @@
+# Author: Lawrence MH LO
+# Version: 1.2 beta
+# Date: 2022 / 11 / 13
+# Description: A simple timer for study or other purposes.
+
 module StudyTimers
 using WAV
-#the wav package must be installed: https://juliapackages.com/p/wav
-#A simple timer for study.
-#On your terminal, run: $julia StudyTimers.jl
-#and then input, say, 5, to have 5 minutes of rest before the program beeps;
-#or input, say, 30, to have 30 minutes of study before the program beeps.
 
-    function TonePlayer()
-        # println("\007\007\007\007\007\007")
-        fs = 7e3
-        t = 0.0:1/fs:prevfloat(0.3)
-        f = 8e2 
-        y = sin.(2pi * f * t) * 0.1 
+# **** Usage guide ***
+# The WAV package must be installed: https://juliapackages.com/p/wav
+# Instructions for adding WAV package: https://docs.julialang.org/en/v1/stdlib/Pkg/
+# On your terminal, run: $julia StudyTimers.jl
+# and then input, say, 5, to have 5 minutes of rest before the program beeps;
+# or input, say, 30, to have 30 minutes of study before the program beeps.
+
+function TonePlayer()
+    # obsolete: println("\007\007\007\007\007\007")
+    # the WAV package must be installed: https://juliapackages.com/p/wav
+    fs = 6e3                    # sampling frequency / number of samples per second in a beep
+    t = 0.0:1/fs:prevfloat(1.0) # time range (with steps) of a beep; 1/fs: the interval between the samples.
+    f = 8e2                     # frequency in Hz (X cycles in 1 second)
+    a = 0.1                     # amplitude 
+    y = sin.(2pi * f * t) * a 
+    for i = 1:10
         wavplay(y, fs) 
-        wavplay(y, fs) 
-        wavplay(y, fs)
-        wavplay(y, fs) 
-        wavplay(y, fs) 
-        wavplay(y, fs)
-        wavplay(y, fs) 
-        wavplay(y, fs) 
-        wavplay(y, fs)
     end
+end
 
-    function MinToSec(min) 
-        sec = min * 60
-        return sec
-    end
+# Declaring more functions 
+MinToSec(min) = min * 60
+CallBack(timer) = TonePlayer()
 
-    function CallBack(timer)
-        TonePlayer()
-    end
+#interface 
+println("Enter the duration in minutes before the program beeps")
+println("(as one non-negative decimal):")
+input = readline()
+duration = parse(Float64, input)
 
-    input = readline()
-    duration = parse(Float64, input)
-
-    t = Timer(CallBack, MinToSec(duration))
-    wait(t)
+#Run timer
+t = Timer(CallBack, MinToSec(duration))
+wait(t)
 
 end
